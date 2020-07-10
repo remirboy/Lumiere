98db84fa8fa90b084772974646b117c5e7d222ab
Mihail Solovev <youoliver@mail.ru>
2020-07-10 15:49:22 +0400

    Relationship between the seance and the reserved seat

--- Lumiere/Models/FilmSeance.cs
+++ Lumiere/Models/FilmSeance.cs
@@ -18,5 +18,6 @@ namespace Lumiere.Models
         public int Price { get; set; }
         public Guid FilmId { get; set; }
         public Film Film { get; set; }
+        public List<ReservedSeat> ReservedSeats { get; set; }
     }
 }
