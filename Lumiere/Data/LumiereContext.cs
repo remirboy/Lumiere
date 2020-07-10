6223628c39acd03b2ce5286ba106a502bfd3bc39
Mihail Solovev <youoliver@mail.ru>
2020-07-10 16:24:33 +0400

    Added tables to the database

--- Lumiere/Data/LumiereContext.cs
+++ Lumiere/Data/LumiereContext.cs
@@ -13,6 +13,9 @@ namespace Lumiere.Data
         }
 
         public DbSet<Film> Films { get; set; }
+        public DbSet<FilmPoster> Posters { get; set; }
+        public DbSet<FilmTrailer> Trailers { get; set; }
+        public DbSet<FilmSeance> Seances { get; set; }
         public DbSet<ReservedSeat> ReservedSeats { get; set; }
         public DbSet<FilmFeedback> FilmFeedbacks { get; set; }
     }
