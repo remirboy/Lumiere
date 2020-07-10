1aebe72ac3c9103d1eb3aa50d2bc22370ad1cbba
Mihail Solovev <youoliver@mail.ru>
2020-07-10 16:28:09 +0400

    Added CRUD interface

--- Lumiere/Repositories/IFilmRepository.cs
+++ Lumiere/Repositories/IFilmRepository.cs
@@ -1,17 +1,9 @@
 ﻿using Lumiere.Models;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Lumiere.Repositories
 {
-    public interface IFilmRepository
+    public interface IFilmRepository : ICrud<Film>
     {
-        Task CreateAsync(Film film);
-        Task UpdateAsync(Film film);
-        Task DeleteAsync(Film film);
-        IEnumerable<Film> GetAll();
-        Task<Film> GetByIdAsync(Guid id);
-        Task<Film> GetByNameAsync(string name);
+        
     }
 }
