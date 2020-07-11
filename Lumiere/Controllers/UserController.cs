701b074524d1878f989c4a346b6fdd488839d153
Mihail Solovev <youoliver@mail.ru>
2020-07-11 14:16:34 +0400

    Added functionality for viewing the user profile

---
+++ Lumiere/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lumiere.Models;
+using Lumiere.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lumiere.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Index(string id)
+        {
+            User user = await _userRepository.GetByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            return View(user);
+        }
+    }
+}
