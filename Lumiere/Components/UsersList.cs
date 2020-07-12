2f6ea4702cfe0f8384fe17432d72b80347025604
Mihail Solovev <youoliver@mail.ru>
2020-07-12 17:26:30 +0400

    The list of users in the admin panel is displayed using the component

---
+++ Lumiere/Components/UsersList.cs
@@ -0,0 +1,26 @@
+﻿using Lumiere.Models;
+using Lumiere.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lumiere.Components
+{
+    public class UsersList : ViewComponent
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersList(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<User> users = _userRepository.GetAll().ToList();
+
+            return View("UsersList", users);
+        }
+    }
+}
