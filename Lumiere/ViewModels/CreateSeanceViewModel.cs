59f6815c2cefe44629d034d99c8108928b33442e
Mihail Solovev <youoliver@mail.ru>
2020-07-13 12:53:48 +0400

    Added a form for adding sessions to movies

---
+++ Lumiere/ViewModels/CreateSeanceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lumiere.ViewModels
+{
+    public class CreateSeanceViewModel
+    {
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public int Price { get; set; }
+    }
+}
